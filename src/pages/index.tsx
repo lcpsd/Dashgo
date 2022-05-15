b6de3e06dd9fc7407137355fd21de7a54a37106d
Lucas Cardoso <lucao@MacBook-Air-de-Elika.local>
2022-05-14 23:14:51 -0400

    chakra config and signIn screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,37 @@
-export default function Home() {
+import { Button, Flex, Stack } from "@chakra-ui/react";
+
+import { Input } from "../components/form/input";
+
+export default function SignIn() {
   return (
-    <h1>OK</h1>
+    <Flex
+      w={'100vw'}
+      h={'100vh'}
+      align={'center'}
+      justify={'center'}>
+        <Flex
+          as="form"
+          width={"100%"}
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDir="column"
+          >
+            <Stack spacing={2}>
+              <Input type="email" name="email" label="E-mail"/>
+              <Input type="password" name="password" label="Senha"/>
+
+              <Button
+                type="submit"
+                mt="6"
+                colorScheme="pink"
+                size="lg"
+                >
+                  Entrar
+                </Button>
+            </Stack>
+        </Flex>
+      </Flex>
   )
 }
