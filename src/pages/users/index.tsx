88ef648c5558336fe0ff5c7690dd026fda3e7c21
dev <dev@rnamob002.rnatec.com.br>
2022-06-27 11:02:55 -0400

    useUsers hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -7,27 +7,11 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { api } from "../../services/axios/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UsersList(){
 
-    const {isLoading, isFetching, error, data} = useQuery('users', async () => {
-        const {data} = await api.get('users')
-
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric'
-                })
-            }
-        })
-        
-        return users
-    })
+    const {isLoading, isFetching, error, data} = useUsers()
 
     const isWideVersion = useBreakpointValue({
         base: false,
